506b15716431086a4ca0d4ebe2781503dc3dab95
qianchaochen <qianchaochen@126.com>
2020-05-07 19:24:42 +0800

    添加路由网关微服务组件

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/qcc/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,20 @@
+package com.qcc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * TODO
+ *
+ * @author qianchaochen
+ * @date 2020/5/7 19:15
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
